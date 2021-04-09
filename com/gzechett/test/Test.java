557312bdfad844fb4d6dfb83834b563442656ea0
G-Zechetti <77728053+G-Zechetti@users.noreply.github.com>
2021-04-09 10:54:03 +0100

    Update Test.java

--- com/gzechett/test/Test.java
+++ com/gzechett/test/Test.java
@@ -3,7 +3,7 @@ package com.gzechett.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World ++ test");
+		System.out.println("Hello World ++ test.version2");
 
 	}
 
