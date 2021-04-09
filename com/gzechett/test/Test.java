6f7ce7d26154c3efee430ac58a996741c90a4a28
G-Zechetti <77728053+G-Zechetti@users.noreply.github.com>
2021-04-09 11:41:41 +0100

    Update Test.java

--- com/gzechett/test/Test.java
+++ com/gzechett/test/Test.java
@@ -4,7 +4,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World ++ test.version2 - verify pollSCM per minute");
-
+		for(int i = 0; i <= 10;i++)
+		{
+			System.out.println("This number: " + i);
+			System.out.println("This number but *2: " + i*2);
+		}
 	}
 
 }
