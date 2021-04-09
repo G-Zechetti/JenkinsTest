71ccdf41c88fd928f8171bde3fabfb5966c3ed87
G-Zechetti <77728053+G-Zechetti@users.noreply.github.com>
2021-04-09 11:37:37 +0100

    Update Test.java

--- com/gzechett/test/Test.java
+++ com/gzechett/test/Test.java
@@ -3,7 +3,7 @@ package com.gzechett.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World ++ test.version2");
+		System.out.println("Hello World ++ test.version2 - verify pollSCM per minute");
 
 	}
 
